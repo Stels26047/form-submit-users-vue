172173c8d24a8749e86fa602fdff177bcad80adf
StelsMaster <sidorukivan1@gmail.com>
2025-01-08 18:08:43 +0500

    add directory dist

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/form-submit-users-vue/dist/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
